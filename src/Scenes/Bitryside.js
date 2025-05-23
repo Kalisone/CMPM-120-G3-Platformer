b53f26868dc90f38dbe92bb07ad5470ee1b67444
Ethan Morelos <etmorelo@ucsc.edu>
2025-05-22 23:27:18 -0700

    sound and particle bug fixes

--- src/Scenes/Bitryside.js
+++ src/Scenes/Bitryside.js
@@ -5,16 +5,17 @@ class Bitryside extends Phaser.Scene {
 
     init() {
         // variables and settings
-        this.ACCELERATION = 1200;
+        this.ACCELERATION = 800;
         this.DRAG = 2400;
         this.physics.world.gravity.y = 1500;
-        this.JUMP_VELOCITY = -600;
+        this.JUMP_VELOCITY = -480;
         this.MAX_SPEED = 240;
         this.PARTICLE_VELOCITY = 50;
         this.SCALE = SCALE;
 
         this.wasInAir = this.inAir = false;
         this.numKeys = 0;
+        this.stepCounter = 0;
     }
 
     preload(){
@@ -124,6 +125,107 @@ class Bitryside extends Phaser.Scene {
         }
         /* END CAMERAS SETUP */
 
+        
+        /* **** **** **** **** **** ****
+         * CREATE VFX
+         **** **** **** **** **** **** */
+        this.load.setPath("../assets/particles/");
+        /* Anims, by variable in my.vfx */
+        my.vfx.keyAnim = this.anims.create({
+            key: "keyAnim",
+            frames: [
+                {key: "kenny-particles", frame: "flare_01.png"},
+                {key: "kenny-particles", frame: "light_01.png"},
+                {key: "kenny-particles", frame: "light_02.png"},
+                {key: "kenny-particles", frame: "light_03.png"}
+            ],
+            duration: 300,
+            frameRate: 10
+        });
+
+        my.vfx.bubblingAnim = this.anims.create({
+            key: "bubblingAnim",
+            frames: [
+                {key: "kenny-particles", frame: "smoke_04.png"},
+                {key: "kenny-particles", frame: "circle_01.png"},
+                {key: "kenny-particles", frame: "smoke_07.png"},
+                {key: "kenny-particles", frame: "circle_04.png"},
+                {key: "kenny-particles", frame: "smoke_08.png"}
+            ],
+            duration: 300,
+            frameRate: 10
+        });
+
+        my.vfx.landingAnim = this.anims.create({
+            key: "landingAnim",
+            frames: [
+                {key: "kenny-particles", frame: "smoke_01.png"},
+                {key: "kenny-particles", frame: "smoke_02.png"},
+                {key: "kenny-particles", frame: "smoke_03.png"},
+                {key: "kenny-particles", frame: "smoke_04.png"},
+                {key: "kenny-particles", frame: "smoke_05.png"},
+                {key: "kenny-particles", frame: "smoke_06.png"},
+                {key: "kenny-particles", frame: "smoke_07.png"},
+                {key: "kenny-particles", frame: "smoke_08.png"},
+                {key: "kenny-particles", frame: "smoke_09.png"}
+            ],
+            duration: 150
+        });
+
+        /* Particles */
+        my.vfx.particleKey = this.add.particles(0, 0, "kenny-particles", {
+            anim: "keyAnim",
+            scale: {start: 0.03, end: 0.2},
+            frequency: my.vfx.keyAnim.msPerFrame,
+            lifespan: my.vfx.keyAnim.duration,
+            alpha: {start: 0.2, end: 0.1},
+            blendMode: "ADD"
+        }).stop();
+
+        my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
+            frame: ["smoke_03.png", "smoke_09.png"],
+            random: true,
+            scale: {start: 0.02, end: 0.04},
+            maxAliveParticles: 8,
+            lifespan: 150,
+            gravityY: -400,
+            alpha: {start: 0, end: 0.1}
+        }).stop();
+
+        my.vfx.landing = this.add.particles(0, 0, "kenny-particles", {
+            anim: "landingAnim",
+            scale: {start: 0.04, end: 0.1},
+            frequency: my.vfx.landingAnim.msPerFrame,
+            lifespan: my.vfx.landingAnim.duration,
+            gravityY: -400
+        }).stop();
+        /* END CREATE VFX */
+
+        /* **** **** **** **** **** ****
+         * CREATE SFX
+         **** **** **** **** **** **** */
+        this.load.setPath("../assets/audio/");
+        my.sfx.jump = [
+            this.sound.add("jump", {volume: 0.6}),
+            this.sound.add("up", {volume: 0.6})
+        ];
+
+        my.sfx.key = [
+            this.sound.add("keyUp", {volume: 0.4}),
+            this.sound.add("keyTone", {volume: 0.4})
+        ];
+
+        my.sfx.steps = [
+            this.sound.add("stepHeavy_0", {volume: 0.8}),
+            this.sound.add("stepMedium_0", {volume: 0.8})
+        ];
+
+        my.sfx.landing = [
+            this.sound.add("stepHard_0", {volume: 0.6}),
+            this.sound.add("stepMedium_0", {volume: 0.8})
+        ];
+        /* END CREATE SFX */
+
         // ANIMATED TILES PLUGIN
         this.animatedTiles.init(this.map);
         
@@ -132,6 +234,7 @@ class Bitryside extends Phaser.Scene {
     }
 
     update(){
+        this.stepCounter++;
         /* **** **** **** **** **** ****
          * PLAYER MOVEMENT
          **** **** **** **** **** **** */
@@ -178,7 +281,7 @@ class Bitryside extends Phaser.Scene {
             my.vfx.walking.stop();
         }
 
-        // [^] JUMPING
+        // [^] IN AIR
         if(!my.sprite.player.body.blocked.down) {
             my.sprite.player.anims.play('jump');
             my.vfx.walking.stop();
@@ -189,17 +292,31 @@ class Bitryside extends Phaser.Scene {
             this.wasInAir = this.inAir;
             this.inAir = false;
         }
+
+        // [^] JUMPING
         if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
             my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
-            this.jump1.play();
-            this.jump2.play();
+            for(let sound of my.sfx.jump){
+                sound.play();
+            }
         }
 
+        // [^] LANDING
         if(this.inAir === false && this.wasInAir === true){
             my.vfx.landing.emitParticleAt(my.sprite.player.x, my.sprite.player.y + (my.sprite.player.displayHeight / 2));
 
-            this.hardStep.play();
-            this.softStep.play();
+            for(let sound of my.sfx.landing){
+                sound.play();
+            }
+        }
+    }
+
+    fxPlayerWalk(){
+        my.vfx.walking.start();
+
+        if(this.stepCounter >= 15){
+            my.sfx.steps[Math.round(Math.random() * (my.sfx.steps.length-1))].play();
+            this.stepCounter = 0;
         }
     }
 }
